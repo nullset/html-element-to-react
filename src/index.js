8fec2c2648aaa8273c38a4dfa6727609bc0aeacb
Nathan Wright <nathan@brandalism.com>
2022-06-11 15:20:46 -0600

    better handling of all element attributes, not just src and className

--- src/index.js
+++ src/index.js
@@ -1,73 +1,85 @@
 import React from "react";
 
 function camelCase(s, delimiter) {
-    let a = s.split(delimiter);
-    return (
-        delimiter + a[1].slice(0, 1).toUpperCase() + a[1].slice(1, a[1].length)
-    );
+  let a = s.split(delimiter);
+  return (
+    delimiter + a[1].slice(0, 1).toUpperCase() + a[1].slice(1, a[1].length)
+  );
 }
 
 function getProps(el) {
-    let props = {};
+  let props = {};
 
-    let events = {};
-    for (let k in el) {
-        if (/^on/.test(k) && el[k]) {
-            events[camelCase(k, "on")] = el[k];
-        }
+  let events = {};
 
-        if (k === "className" && el[k]) {
-            props[k] = el[k];
-        }
-
-        if (k === "src" && el[k]) {
-            props[k] = el[k];
-        }
+  //   Set any inline events.
+  for (let k in el) {
+    if (/^on/.test(k) && el[k]) {
+      events[camelCase(k, "on")] = el[k];
     }
-    props = Object.assign(props, events);
-    return props;
+  }
+
+  //   Set values for attributes.
+  for (let attr in el.attributes) {
+    if (attr.name === "style") {
+      const style = {};
+      Array.from(el.style).forEach((key) => {
+        style[key] = /^--/.test(key)
+          ? el.style.getPropertyValue(key)
+          : el.style[key];
+      });
+      props.style = style;
+    } else if (attr.name === "class") {
+      props.className = attr.value;
+    } else {
+      props[attr.name] = attr.value;
+    }
+  }
+
+  props = Object.assign(props, events);
+  return props;
 }
 
 function getChildren(elements) {
-    let children = [];
-    if (!elements) return children;
-    for (let i = 0; i < elements.length; i++) {
-        let el = elements[i];
-        let props = { ...getProps(el) };
+  let children = [];
+  if (!elements) return children;
+  for (let i = 0; i < elements.length; i++) {
+    let el = elements[i];
+    let props = { ...getProps(el) };
 
-        if (el.childNodes.length) {
-            children.push(
-                React.createElement(
-                    el.tagName.toLowerCase(),
-                    props === {} ? null : props,
-                    ...getChildren(el.childNodes)
-                )
-            );
-        } else {
-            if (el.nodeType === 3) {
-                children.push(el.textContent);
-            } else if (el.nodeType === 1) {
-                if (["VIDEO", "IMG", "AUDIO"].includes(el.nodeName)) {
-                    children.push(
-                        React.createElement(
-                            el.tagName.toLowerCase(),
-                            props === {} ? null : props
-                        )
-                    );
-                }
-            }
+    if (el.childNodes.length) {
+      children.push(
+        React.createElement(
+          el.tagName.toLowerCase(),
+          props === {} ? null : props,
+          ...getChildren(el.childNodes)
+        )
+      );
+    } else {
+      if (el.nodeType === 3) {
+        children.push(el.textContent);
+      } else if (el.nodeType === 1) {
+        if (["VIDEO", "IMG", "AUDIO"].includes(el.nodeName)) {
+          children.push(
+            React.createElement(
+              el.tagName.toLowerCase(),
+              props === {} ? null : props
+            )
+          );
         }
+      }
     }
+  }
 
-    return children;
+  return children;
 }
 
 export const convert = function (domElement) {
-    if (!domElement) return null;
-    let props = { ...getProps(domElement) };
-    return React.createElement(
-        domElement.tagName.toLowerCase(),
-        props === {} ? null : props,
-        ...getChildren(domElement.childNodes)
-    );
+  if (!domElement) return null;
+  let props = { ...getProps(domElement) };
+  return React.createElement(
+    domElement.tagName.toLowerCase(),
+    props === {} ? null : props,
+    ...getChildren(domElement.childNodes)
+  );
 };
